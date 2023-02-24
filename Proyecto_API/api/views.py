e907815b0c3c2d6b59847d4dc9f938d36919a21d
RaulOrtiz99 <raulalbertoortiz290199@hotmail.com>
2023-02-24 04:19:41 -0400

    agregando configuraciones para las peticionesI

--- Proyecto_API/api/views.py
+++ Proyecto_API/api/views.py
@@ -1,15 +1,39 @@
-
+from django.http.response import JsonResponse
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
 from django.views import View
+from .models import *
+
 
 # Create your views here.
 #esto es para procesar las respuestas
 class CompanyView(View):
     
+    #este metodo es para procesar las respuestas
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
+    
     def get(self,request):
-        pass 
+        companies = list(Company.objects.values())
+
+        if len(companies)>0:
+            datos = {'message':"Success",'companies':companies}
+        else:
+            datos = {'message':"Companies not found..."}
+        
+        return JsonResponse(datos)
+
+
+        
     
     def post(self,request):
-        pass
+        datos = {'message':"Success"}
+        return JsonResponse(datos)
+    
+    
+    
+    
     
     def put(self,request):
         pass
