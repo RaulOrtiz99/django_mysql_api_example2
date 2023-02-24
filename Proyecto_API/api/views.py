dd861d2e8ccf2637ec9738639f925b53e94efbd8
RaulOrtiz99 <raulalbertoortiz290199@hotmail.com>
2023-02-24 04:01:31 -0400

    agregando las configuraciones tanto para las rutas como para la API

--- Proyecto_API/api/views.py
+++ Proyecto_API/api/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+
+from django.views import View
 
 # Create your views here.
+#esto es para procesar las respuestas
+class CompanyView(View):
+    
+    def get(self,request):
+        pass 
+    
+    def post(self,request):
+        pass
+    
+    def put(self,request):
+        pass
+    
+    def delete(self,request):
+        pass
+    
